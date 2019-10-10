823f9971d4763252f954554dd3b4e1b4b7a38bd5
anhvutrinhdoan <50778028+anhvutrinhdoan@users.noreply.github.com>
2019-10-09 19:30:46 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -17,12 +17,13 @@ var dist = [[2,10,13],
 			[9,9,20],
 			[6,21,9],
 			[12,29,36]];
-//arrays that contain the pops and districts			
+//arrays that contain the pops and districts
 var globalPopulation = new Array();
 var globalDistrictContainer = new Array();
 //ages are 0-18, 19-35, 36-55, 56-66, 67+
 var demographics = [0.15,0.25,0.30,0.20,0.10];
-var sexratio = [0.49,0.51] //female 49%, male 51%
+var sexRatio = [0.49,0.51]; //female 49%, male 51%
+var sexes=["female","male"];
 
 //POLITICAL STUFF
 var partyAffiliation = ["Democratic","Republican"];
@@ -89,7 +90,11 @@ class Pop{
 	}
 	reportSex(){
 		var popsex = this.sex;
-		return sex;
+		return popsex;
+	}
+	reportAge(){
+		var popage = this.age;
+		return popage;
 	}
 	addCash(money){
 		this.cash += money;
@@ -116,14 +121,30 @@ class District{
 			var counter = 0;
 			for (var d=0;d<this.popsInhabiting.length;d++){
 				//...if pop reports party affiliation 'x', look through list of parties for 'x', then add 1 to tally for that party
-				//Poll the pop. If the pop's party is the same as the x'th party in the list of all parties, add 1 to that count				
+				//Poll the pop. If the pop's party is the same as the x'th party in the list of all parties, add 1 to that count
 				if(this.popsInhabiting[d][0].reportPartyAllegiance()==partyAffiliation[p]){
-					counter++;					
-				}	
+					counter++;
+				}
 			}
-			localPartyRegistry.push(counter);			
+			localPartyRegistry.push(counter);
 		}
 		return localPartyRegistry;
+	}
+	reportSexes(){
+		var localSexRegistry=[];
+		for(var p=0;p<sexes.length;p++){
+			//for every pop in district...
+			var counter = 0;
+			for (var d=0;d<this.popsInhabiting.length;d++){
+				//...if pop reports sex 'x', look through list of sexes for 'x', then add 1 to tally for that sex
+				//Poll the pop. If the pop's sex is the same as the x'th sex in the list of all sexes, add 1 to that count
+				if(this.popsInhabiting[d][0].reportSex()==sexes[p]){
+					counter++;
+				}
+			}
+			localSexRegistry.push(counter);
+		}
+		return localSexRegistry;
 	}
 }
 
@@ -135,30 +156,30 @@ class Party{
 //Look through all districts. For each district, generate a new pop object for each social stratum
 function generateInitialPops(){
 	//Iterate through every district
-	for (var i=0; i < dist.length;i++){		
+	for (var i=0; i < dist.length;i++){
 		//Iterate through each pop stratum in the n'th district
 		//Iterate through every pop in that stratum
-		for (var k=0;k<dist[i][0];k++){			
+		for (var k=0;k<dist[i][0];k++){
 			var stratum = 0;
 			var uniqueid = Math.floor(Math.random()*100000)+""+(i+1) + "" + 0 + "" + k;
-			var popObject = new Pop(uniqueid,stratum,randomizePartyAffiliation(stratum),randomizeAge(),randomizeJobIndustry(),randomizeGeneral(),randomizeGeneral(),randomizeGeneral(),0.01,1,1,0,0, randomizeStartingCash(stratum),0,i,randomizeGeneral());
+			var popObject = new Pop(uniqueid,stratum,randomizePartyAffiliation(stratum),randomizeAge(),randomizeJobIndustry(),randomizeGeneral(),randomizeGeneral(),randomizeGeneral(),0.01,1,1,0,0, randomizeStartingCash(stratum),0,i,randomizeGeneral(),randomizeSexes());
 			globalPopulation.push(popObject);
 		}
-		for (var k=0;k<dist[i][1];k++){			
+		for (var k=0;k<dist[i][1];k++){
 			var stratum = 1;
 			var uniqueid = Math.floor(Math.random()*100000)+""+(i+1) + "" + 1 + "" + k;
-			var popObject = new Pop(uniqueid,stratum,randomizePartyAffiliation(stratum),randomizeAge(),randomizeJobIndustry(),randomizeGeneral(),randomizeGeneral(),randomizeGeneral(),0.01,1,1,0,0, randomizeStartingCash(stratum),0,i,randomizeGeneral());
+			var popObject = new Pop(uniqueid,stratum,randomizePartyAffiliation(stratum),randomizeAge(),randomizeJobIndustry(),randomizeGeneral(),randomizeGeneral(),randomizeGeneral(),0.01,1,1,0,0, randomizeStartingCash(stratum),0,i,randomizeGeneral(),randomizeSexes());
 			globalPopulation.push(popObject);
-		}	
-		for (var k=0;k<dist[i][2];k++){			
+		}
+		for (var k=0;k<dist[i][2];k++){
 			var stratum = 2;
 			var uniqueid = Math.floor(Math.random()*100000)+""+(i+1) + "" + 2 + "" + k;
-			var popObject = new Pop(uniqueid,stratum,randomizePartyAffiliation(stratum),randomizeAge(),randomizeJobIndustry(),randomizeGeneral(),randomizeGeneral(),randomizeGeneral(),0.01,1,1,0,0, randomizeStartingCash(stratum),0,i,randomizeGeneral());
+			var popObject = new Pop(uniqueid,stratum,randomizePartyAffiliation(stratum),randomizeAge(),randomizeJobIndustry(),randomizeGeneral(),randomizeGeneral(),randomizeGeneral(),0.01,1,1,0,0, randomizeStartingCash(stratum),0,i,randomizeGeneral(),randomizeSexes());
 			globalPopulation.push(popObject);
-		}						
+		}
 	}
 	populateDistricts();
-}		
+}
 //Initialize game state with all districts assigned pops
 function populateDistricts(){
 	for (var n=0;n<dist.length;n++){
@@ -170,18 +191,18 @@ function populateDistricts(){
 			}
 		}
 		var newDistrict = new District(n,copier);
-		globalDistrictContainer.push(newDistrict);		
+		globalDistrictContainer.push(newDistrict);
 	}
 	//clear it out now that the districts are populated
 	globalPopulation = [];
 }
-	
+
 //Looping through a weighted random array. Given weights returns the i'th entry of the array
 
 function loopArray(anArray){
 	var i=0, breakout=false, n=0, r=Math.random();
 	while (breakout==false){
-		n+= anArray[i];
+		n+=anArray[i];
 		if(r<=n){
 			breakout = true;
 			return i;
@@ -192,18 +213,24 @@ function loopArray(anArray){
 	}
 }
 
+//Randomly generate sexes
+function randomizeSexes(){
+	var gen = loopArray(sexRatio);
+	return sexes[gen];
+}
+
 //Randomly generate party affiliations for an individual pop
 function randomizePartyAffiliation(stratum){
 	if(stratum ==0){
-		var gen = loopArray(partyWeightsForUpperClass);		
+		var gen = loopArray(partyWeightsForUpperClass);
 		return partyAffiliation[gen];
 	}
 	if(stratum ==1){
-		var gen = loopArray(partyWeightsForMiddleClass);		
+		var gen = loopArray(partyWeightsForMiddleClass);
 		return partyAffiliation[gen];
 	}
 	if(stratum ==2){
-		var gen = loopArray(partyWeightsForLowerClass);		
+		var gen = loopArray(partyWeightsForLowerClass);
 		return partyAffiliation[gen];
 	}
 }
@@ -232,7 +259,7 @@ function randomizeAge(){
 //Randomly generate job industry for each individual pop
 function randomizeJobIndustry(){
 	var x = Math.floor(Math.random()*17);
-	return jobIndustry[x];	
+	return jobIndustry[x];
 }
 
 //Random math
@@ -259,29 +286,30 @@ function popupwindow(n){
 	var makeMyPopupHeader = document.createElement("div");
 	var makeCloseBox = document.createElement("div");
 	var makeActionMenu = document.createElement("div");
-	
+
 	makeMyPopup.className = "popupwindow";
 	makeMyPopup.setAttribute("id","dist"+n);
 	makeMyPopup.innerHTML = "District " + n + "<br/>" + "Pops living here: " + pops[n-1] + "<br/>" + "<img src='oligarchpop.png' class='pop'/>" + "Upper-class pops: " + dist[n-1][0] + "<br/>" + "<img src='whitecollarpop.png' class='pop'/>" + "White collar pops: " + dist[n-1][1]  + "<br/>" + "<img src='workingclasspop.png' class='pop'/>" + "Working-class pops: " + dist[n-1][2] ;
-	document.getElementById("main").appendChild(makeMyPopup);	
+	document.getElementById("main").appendChild(makeMyPopup);
 	var subElement = document.createElement("div");
 	subElement.setAttribute("id","piechart");
-	
+
 	makeMyPopupHeader.setAttribute("id","header");
 	makeMyPopupHeader.onclick = dragElement(document.getElementById("dist"+n));
 	makeCloseBox.setAttribute("id","xbutton");
 	makeCloseBox.onclick = closeanypopupwindow;
 	makeCloseBox.innerHTML = "X";
-	
+
 	makeActionMenu.setAttribute("id","popupwindow_actions");
-	
-	document.getElementById("dist"+n).appendChild(makeMyPopupHeader);	
-	document.getElementById("dist"+n).appendChild(subElement);	
+
+	document.getElementById("dist"+n).appendChild(makeMyPopupHeader);
+	document.getElementById("dist"+n).appendChild(subElement);
 	document.getElementById("dist"+n).appendChild(makeCloseBox);
 	document.getElementById("dist"+n).appendChild(makeActionMenu);
-	
+
 	districtclicked = n;
-	drawChart(dataLoader(n-1));
+	var datalabels = ['Party','Percentage of Vote'];
+	drawChart(dataLoader(datalabels,n-1,0));
 }
 
 function closeanypopupwindow(){
@@ -294,49 +322,64 @@ function closepartywindow(){
 	partyMenuSelected=false;
 }
 //PARTY CONTROL MENU
-
 function openPartyMenu(){
 	if(partyMenuSelected==false){
 		var makePartyMenu = document.createElement("div");
-		makePartyMenu.setAttribute("id","screen_partycontrol");
 		var makeMyPopupHeader = document.createElement("div");
-		makeMyPopupHeader.setAttribute("id","header");	
 		var makeCloseBox = document.createElement("div");
-		makeCloseBox.setAttribute("id","xbutton");		
-		makeCloseBox.innerHTML = "X";
 		var makePartyMenuDesc = document.createElement("div");
+		var makePartyMenuLeaders = document.createElement("div");
+		var makePartyMenuPlatforms = document.createElement("div");
+		var makePartyMenuModifiers = document.createElement("div");
+
+		makePartyMenu.setAttribute("id","screen_partycontrol");
+		makeMyPopupHeader.setAttribute("id","header");
+		makeCloseBox.setAttribute("id","xbutton");
+		makeCloseBox.innerHTML = "X";
 		makePartyMenuDesc.setAttribute("id","screen_partycontrol_party_desc");
 		makePartyMenuDesc.innerHTML = "Party Description";
-		var makePartyMenuLeaders = document.createElement("div");
 		makePartyMenuLeaders.setAttribute("id","screen_partycontrol_party_leaders");
 		makePartyMenuLeaders.innerHTML = "Party Leaders";
-		var makePartyMenuPlatforms = document.createElement("div");
 		makePartyMenuPlatforms.setAttribute("id","screen_partycontrol_party_platforms");
 		makePartyMenuPlatforms.innerHTML = "Party Platforms";
-		document.getElementById("main").appendChild(makePartyMenu);
-		var makePartyMenuModifiers = document.createElement("div");
 		makePartyMenuModifiers.innerHTML = "Active Modifiers";
 		makePartyMenuModifiers.setAttribute("id","screen_partycontrol_party_modifiers");
+
+		document.getElementById("main").appendChild(makePartyMenu);
 		document.getElementById("screen_partycontrol").innerHTML= playerCurrentParty + " Party" + "<br><br>" + "<img src=" + "party_" + playerCurrentParty + ".png>" + "<br>";
 		document.getElementById("screen_partycontrol").appendChild(makeMyPopupHeader);
+
 		makeMyPopupHeader.onclick = dragElement(document.getElementById("screen_partycontrol"));
-		makeCloseBox.onclick = closepartywindow;	
+		makeCloseBox.onclick = closepartywindow;
+
 		document.getElementById("screen_partycontrol").appendChild(makeCloseBox);
-		document.getElementById("screen_partycontrol").appendChild(makePartyMenuDesc);	
-		document.getElementById("screen_partycontrol").appendChild(makePartyMenuLeaders);	
+		document.getElementById("screen_partycontrol").appendChild(makePartyMenuDesc);
+		document.getElementById("screen_partycontrol").appendChild(makePartyMenuLeaders);
 		document.getElementById("screen_partycontrol").appendChild(makePartyMenuModifiers);
 		document.getElementById("screen_partycontrol").appendChild(makePartyMenuPlatforms);
 		partyMenuSelected = true;
-	}	
+	}
 }
 
 //DATA AND FORMATTING
 
-function dataLoader(n){	
-	var dataLabels=[['Party','Percentage of Vote']];
-	var votePoller=globalDistrictContainer[n].reportVotes();
-	for(var z=0;z<partyAffiliation.length;z++){
-		dataLabels.push([partyAffiliation[z],votePoller[z]]);
+function dataLoader(labels,n,x){
+	var dataLabels=[labels];
+	switch(x){
+			//Counting votes
+		case 0:
+			var votePoller=globalDistrictContainer[n].reportVotes();
+			for(var z=0;z<partyAffiliation.length;z++){
+				dataLabels.push([partyAffiliation[z],votePoller[z]]);
+			}
+			break;
+			//counting male or female pops
+		case 1:
+			var sexPoller=globalDistrictContainer[n].reportSexes();
+			for(var z=0;z<sexes.length;z++){
+				dataLabels.push([sexes[z],sexPoller[z]]);
+			}
+			break;
 	}
 	return dataLabels;
 }
@@ -360,20 +403,18 @@ function drawChart(pushedArray) {
 			left:30,
 			top: 20,
 			width: '100%'
-		},	  
+		},
 	};
 	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 	chart.draw(data, options);
 }
 
 //Demographics: Population pyramid
-function popPyramid(){
+function popPyramid(pushedArray){
 	//var data = new google.visualization.DataTable();
 
 	var dataArray = [['Age', 'Female', 'Male']];
-	for(){
-		
-	}
+
 	var data = google.visualization.arrayToDataTable(dataArray);
 	var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
 	var options = {
@@ -436,13 +477,13 @@ function dragElement(elmnt) {
 
 //color the districts with the color of the party that has the highest proportion of voters
 function politicalMapMode(){
-	for(var i = 0; i<dist.length;i++){	
+	for(var i = 0; i<dist.length;i++){
 		var poll = globalDistrictContainer[i].reportVotes();
 		var addall = poll.reduce(sum);
 		var container = [];
 		for (var q=0;q<partyAffiliation.length;q++){
 			container.push(poll[q]/addall);
-		}		
+		}
 		document.getElementById(i).style.backgroundColor=partyColor[container.indexOf(Math.max(...container))];
 		if(partyAffiliation[container.indexOf(Math.max(...container))]==playerCurrentParty){
 				document.getElementById(i).style.outline="2px solid yellow";
@@ -465,7 +506,7 @@ function sum(a,b){
 
 function setDefaultParty(n){
 	playerCurrentParty = partyAffiliation[n];
-	var string = "party_"+partyAffiliation[n]+".png";	
+	var string = "party_"+partyAffiliation[n]+".png";
 	document.getElementById("partyflag").style.backgroundImage='url('+string+')';
 	console.log('party_'+partyAffiliation[n]+'.png');
 }
